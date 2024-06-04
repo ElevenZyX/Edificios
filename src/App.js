660ee244e6efb3ea6509966a0f190d12703a1917
javiera1709 <javiemarin@alumnos.uai.cl>
2024-06-04 13:58:58 -0400

    Mejorar la seguridad de la página

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container } from 'react-bootstrap';
 import Login from './components/Login';
 import Home from './components/Home';
 import Visit from './components/Visit';
 import LanguageSelector from './components/LanguageSelector';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
+import { AuthProvider } from './components/AuthContext';
 
 function App() {
-
   return (
-    <Router>
-      <Container>
-        <LanguageSelector />
-        
-        <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/Visit" element={<Visit/>}/>
-        </Routes>
-      </Container>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Container>
+          <LanguageSelector />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route
+              path="/home"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/visit"
+              element={
+                <PrivateRoute>
+                  <Visit />
+                </PrivateRoute>
+              }
+            />
+            {/* Agrega más rutas protegidas aquí */}
+          </Routes>
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- src/components/Visit.js
+++ src/components/Visit.js
@@ -1,108 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { Form, Button, Container, Row, Col, Alert } from 'react-bootstrap';
-import axios from 'axios';
-import NavBar from './NavBar';
-import Footer from './Footer';
-import { useTranslation } from 'react-i18next';
+import React from 'react';
+import { useAuth } from './AuthContext';
 
 function Visit() {
-  const { t } = useTranslation();
-  const [departments, setDepartments] = useState([]);
-  const [selectedDepartment, setSelectedDepartment] = useState('');
-  const [nombre, setNombre] = useState('');
-  const [fecha, setFecha] = useState('');
-  const [hora, setHora] = useState('');
-  const [message, setMessage] = useState(null);
-
-  useEffect(() => {
-    axios.get('http://localhost:8000/api/departments')
-      .then(response => {
-        setDepartments(response.data);
-      })
-      .catch(error => {
-        setMessage(t('recoveringDptoError'));
-        console.error(t('consoleDptoError'), error);
-      });
-  }, []);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const visita = {
-        departamento: selectedDepartment,
-        nombre,
-        fecha,
-        hora
-      };
-      const response = await axios.post('http://localhost:8000/api/visitas', visita);
-      setMessage('Visita registrada con éxito');
-      setSelectedDepartment('');
-      setNombre('');
-      setFecha('');
-      setHora('');
-      console.log(response.data);
-    } catch (error) {
-      setMessage('Error al registrar la visita');
-      console.error('Error al enviar el formulario:', error);
-    }
-  };
+  const { logout } = useAuth();
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-      <NavBar />
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col lg={6}>
-            {message && <Alert variant={message.startsWith('Error') ? 'danger' : 'success'}>{message}</Alert>}
-            <Form onSubmit={handleSubmit}>
-              <Form.Group controlId="visitasForm.DepartmentSelect">
-                <Form.Label style={{ fontSize: '1.2rem', marginTop: '1.5rem' }}>{t('department')}</Form.Label>
-                <Form.Control as="select" value={selectedDepartment} onChange={e => setSelectedDepartment(e.target.value)} style={{ fontSize: '1.2rem' }}>
-                  <option value="">{t('selectDepartment')}</option>
-                  {departments.map((dept, index) => (
-                    <option key={index} value={dept.Number}>{dept.Number}</option>
-                  ))}
-                </Form.Control>
-              </Form.Group>
-
-              <Form.Group controlId="visitasForm.Nombre">
-                <Form.Label style={{ fontSize: '1.2rem', marginTop: '1.5rem' }}>{t('name')}</Form.Label>
-                <Form.Control
-                  type="text"
-                  value={nombre}
-                  onChange={e => setNombre(e.target.value)}
-                  style={{ fontSize: '1.2rem' }}
-                />
-              </Form.Group>
-
-              <Form.Group controlId="visitasForm.Fecha">
-                <Form.Label style={{ fontSize: '1.2rem', marginTop: '1.5rem' }}>{t('date')}</Form.Label>
-                <Form.Control
-                  type="date"
-                  value={fecha}
-                  onChange={e => setFecha(e.target.value)}
-                  style={{ fontSize: '1.2rem' }}
-                />
-              </Form.Group>
-
-              <Form.Group controlId="visitasForm.Hora">
-                <Form.Label style={{ fontSize: '1.2rem', marginTop: '1.5rem' }}>{t('time')}</Form.Label>
-                <Form.Control
-                  type="time"
-                  value={hora}
-                  onChange={e => setHora(e.target.value)}
-                  style={{ fontSize: '1.2rem' }}
-                />
-              </Form.Group>
-
-              <Button variant="primary" type="submit" className='my-4 btn-lg'>
-                {t('registerVisit')}
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-      <Footer />
+    <div>
+      <h1>Welcome to the Visit Page</h1>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 }
