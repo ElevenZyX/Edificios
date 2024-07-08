e6c6300c33f411291c8101d47db2a2d12298ea46
Diego <diegot9.dt@gmail.com>
2024-07-08 18:04:27 -0400

    mini cambio

--- src/components/axiosConfig.js
+++ src/components/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://proyectobuildingbuddy.azurewebsites.net',
 });
 
 instance.interceptors.request.use(
